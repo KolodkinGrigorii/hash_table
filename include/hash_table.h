c047f91e7df69f732f1f19c218661dd9a1736610
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2024-03-07 19:03:08 +0300

    final

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,20 +1,75 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+
 using namespace std;
 template <typename TypeKey, typename TypeData>
 class HashTable {
 private:
-	vector<pair<TypeKey, TypeData>> *ar;
+	vector < pair < TypeKey, TypeData>>* ar;
 	int length;
 public:
+	class Iterator
+	{
+	private:
+		vector < pair < TypeKey, TypeData>>* ar_;
+		size_t counter_;
+		size_t number_;
+		size_t length_;
+	public:
+		Iterator(vector < pair < TypeKey, TypeData>>* ar, size_t counter, size_t number, size_t length) : ar_(ar), counter_(counter), number_(number), length_(length) {}
+		Iterator& operator++()
+		{
+			if (ar_[counter_].size() == number_ + 1)
+			{
+				counter_++;
+				while (ar_[counter_].size() == 0)
+				{
+					if (counter_ == length_)
+						return *this;
+					number_ = 0;
+					counter_++;
+				}
+			}
+			else
+			{
+				number_++;
+			}
+			return *this;
+		}
+		static Iterator begin(vector < pair < TypeKey, TypeData>>* ar, size_t length)
+		{
+			for (size_t i = 0; i < length; i++)
+			{
+				if (ar[i].size() != 0)
+					return Iterator(ar, i, 0, length);
+			}
+			return Iterator(ar, length, 0, length);
+		}
+		pair<TypeKey, TypeData>& operator*()
+		{
+			return ar_[counter_][number_];
+		}
+		bool operator ==(const Iterator& other)
+		{
+			return (ar_ == other.ar_ && counter_ == other.counter_ && number_ == other.number_);
+		}
+		bool operator !=(const Iterator& other)
+		{
+			return !(*this == other);
+		}
+		pair<TypeKey, TypeData>* operator->()
+		{
+			return &**this;
+		}
+	};
 	HashTable(int n) {
-		ar = new vector<pair<TypeKey, TypeData>>[n];
+		ar = new vector < pair < TypeKey, TypeData>>[n];
 		length = n;
 	}
 	HashTable(const HashTable& ht) {
 		length = ht.length;
-		ar = new vector<pair<TypeKey, TypeData>>[length];
+		ar = new vector < pair < TypeKey, TypeData>>[length];
 		for (int i = 0; i < length; i++) {
 			ar[i] = ht.ar[i];
 		}
@@ -28,7 +83,7 @@ public:
 		else {
 			length = ht.length;
 			delete[] ar;
-			ar = new vector<pair<TypeKey, TypeData>>[length];
+			ar = new vector < pair < TypeKey, TypeData>>[length];
 			for (int i = 0; i < length; i++) {
 				ar[i] = ht.ar[i];
 			}
@@ -40,8 +95,8 @@ public:
 		delete[] ar;
 	}
 	int hash(string s) {
-		char al[]= "abcdefghijklmnopqrstuvwxyz";
-		int sum=0;
+		char al[] = "abcdefghijklmnopqrstuvwxyz";
+		int sum = 0;
 		for (int i = 0; i < s.length(); i++) {
 			char c = s[i];
 			for (int j = 0; j < 26; j++) {
@@ -50,10 +105,10 @@ public:
 				}
 			}
 		}
-		return sum%length;
+		return sum % length;
 	}
 	int hash(int i) {
-		return i%length;
+		return i % length;
 	}
 	auto insert(TypeKey key, const TypeData& data) {
 		int pos = hash(key);
@@ -64,14 +119,14 @@ public:
 			}
 		}
 	}
-	auto find(const TypeKey& key) {
+	Iterator find(const TypeKey& key) {
 		int pos = hash(key);
 		for (auto it = ar[pos].begin(); it != ar[pos].end(); it++) {
 			if (it->first == key) {
-				return it;
+				return Iterator(ar, pos, it - ar[pos].begin(), length);
 			}
 		}
-		return ar[length - 1].end();
+		return Iterator(ar, length, 0, length);
 	}
 	bool remove(const TypeKey& key) {
 		int pos = hash(key);
@@ -85,27 +140,25 @@ public:
 	}
 	TypeData& operator[](const TypeKey& key) {
 		int pos = hash(key);
-		for (auto it = ar[pos].begin(); it != ar[pos].end(); it++) {
+		for (auto it = ar[pos].begin(); it != ar[pos].end(); ++it) {
 			if (it->first == key) {
 				return it->second;
 			}
 		}
-		throw 'FALL';
+		throw std::runtime_error("Invalid key!");
 	}
-	auto begin(){
-		int i = 0;
-		while (ar[i].size() == 0) {
-			i++;
-		}
-		return ar[i].begin();
+
+	Iterator begin() {
+		return Iterator::begin(ar, length);
 	}
-	auto end() {
-		return ar[length-1].end();
+	Iterator end() {
+		return Iterator(ar, length, 0, length);
 	}
+
 	friend std::ostream& operator<<(std::ostream& out, const HashTable& v) {
 		for (int i = 0; i < v.length; i++) {
 			for (auto it = v.ar[i].begin(); it != v.ar[i].end(); it++) {
-				cout << i << " "<< it->first << " " << it->second << endl;
+				cout << i << " " << it->first << " " << it->second << endl;
 			}
 		}
 		return out;
--- test/test_table.cpp
+++ test/test_table.cpp
@@ -1,17 +1,16 @@
 #pragma once
-
-
 #include "hash_table.h"
 #include <gtest.h>
 using namespace std;
-TEST(HashTableTest, insert_hash_table) { //не работает
+
+TEST(HashTableTest, insert_hash_table) {
 	HashTable<string, int> ht(10);
 	int res1 = ht.insert("Hello", 1)->second;
 	int et1 = 1;
 	EXPECT_EQ(et1, res1);
 }
 
-TEST(HashTableTest, copy_equal_tables) { //не работает
+TEST(HashTableTest, copy_equal_tables) {
 	HashTable<string, int> ht(10);
 	int et1 = ht.insert("Hello", 1)->second;
 	HashTable<string, int> eht(ht);
@@ -19,7 +18,7 @@ TEST(HashTableTest, copy_equal_tables) { //не работает
 	EXPECT_EQ(et1, res);
 }
 
-TEST(HashTableTest, InsertAndFind) {//работает
+TEST(HashTableTest, InsertAndFind) {
 	HashTable<string, int> ht(5);
 	ht.insert("apple", 10);
 	ht.insert("banana", 20);
@@ -29,7 +28,7 @@ TEST(HashTableTest, InsertAndFind) {//работает
 	EXPECT_EQ(ht.find("orange")->second, 30);
 }
 
-TEST(HashTableTest, Remove) {//работает
+TEST(HashTableTest, Remove) {
 	HashTable<string, int> ht(5);
 	ht.insert("apple", 10);
 	ht.insert("banana", 20);
@@ -37,7 +36,7 @@ TEST(HashTableTest, Remove) {//работает
 	EXPECT_FALSE(ht.remove("orange"));
 }
 
-TEST(HashTableTest, BracketOperator) { //реагирует на 84 строку
+TEST(HashTableTest, BracketOperator) { 
 	HashTable<string, int> ht(5);
 	ht.insert("apple", 10);
 	ht.insert("banana", 20);
@@ -45,21 +44,20 @@ TEST(HashTableTest, BracketOperator) { //реагирует на 84 строку
 	EXPECT_EQ(ht["banana"], 20);
 }
 
-/*TEST(HashTableTest, Iterator) { //реагирует на вектор
+TEST(HashTableTest, Iterator) { 
 	HashTable<string, int> ht(5);
 	ht.insert("apple", 10);
 	ht.insert("banana", 20);
 	ht.insert("orange", 30);
 	int sum = 0;
-	for (auto it = ht.begin(); it != ht.end(); it++)
+	for (auto it = ht.begin(); it != ht.end(); ++it)
 	{
 		sum += it->second;
 	}
 	EXPECT_EQ(sum, 60);
-}*/
+}
 
-
-TEST(HashTable, Insert) { //84 строка
+TEST(HashTable, Insert) { 
 	HashTable<string, int> ht(10);
 	ht.insert("apple", 1);
 	ht.insert("banana", 2);
@@ -70,7 +68,7 @@ TEST(HashTable, Insert) { //84 строка
 	EXPECT_EQ(ht["cherry"], 3);
 }
 
-TEST(HashTable, Find) { //присваивание
+TEST(HashTable, Find) { 
 	HashTable<string, int> ht(10);
 	ht.insert("apple", 1);
 	ht.insert("banana", 2);
@@ -81,20 +79,18 @@ TEST(HashTable, Find) { //присваивание
 	EXPECT_EQ(ht.find("cherry")->second, 3);
 }
 
-TEST(HashTable, Remove) {//скобки
+TEST(HashTable, Remove) {
 	HashTable<string, int> ht(10);
 	ht.insert("apple", 1);
 	ht.insert("banana", 2);
 	ht.insert("cherry", 3);
 
-	ht.remove("apple");
-
-	EXPECT_EQ(ht.find("apple"), ht.end());
+	EXPECT_TRUE(ht.remove("apple"));
 	EXPECT_EQ(ht["banana"], 2);
 	EXPECT_EQ(ht["cherry"], 3);
 }
 
-/*TEST(HashTable, Iterator) { //вектор
+TEST(HashTable, Iterator) {
 	HashTable<string, int> ht(10);
 	ht.insert("apple", 1);
 	ht.insert("banana", 2);
@@ -104,11 +100,11 @@ TEST(HashTable, Remove) {//скобки
 	EXPECT_EQ(it->first, "banana");
 	EXPECT_EQ(it->second, 2);
 
-	it++;
-	EXPECT_EQ(it->first, "banana");
-	EXPECT_EQ(it->second, 2);
-
-	it++;
+	++it;
 	EXPECT_EQ(it->first, "cherry");
 	EXPECT_EQ(it->second, 3);
-}*/
+
+	++it;
+	EXPECT_EQ(it->first, "apple");
+	EXPECT_EQ(it->second, 1);
+}
