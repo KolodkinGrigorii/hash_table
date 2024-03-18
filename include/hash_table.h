9e9d9f5b26b0051783de163cb75c3128cf23253d
Григорий <grixakol@yandex.ru>
2024-03-18 21:07:46 +0300

    char+vector<int>

--- include/hash_table.h
+++ include/hash_table.h
@@ -84,8 +84,10 @@ public:
 			length = ht.length;
 			delete[] ar;
 			ar = new vector < pair < TypeKey , TypeData>>[length];
-			for (int i = 0; i < length; i++) {
-				ar[i] = ht.ar[i];
+			for (int i = 0; i < ht.length; i++) {
+				for (auto it = ht.ar[i].begin(); it != ht.ar[i].end(); it++) {
+					this->insert(it->first, it->second);
+				}
 			}
 		}
 		return *this;
@@ -107,6 +109,19 @@ public:
 		}
 		return sum % length;
 	}
+	int hash(char c) {
+		char al[] = "abcdefghijklmnopqrstuvwxyz";
+		int sum = 0;
+		for (int j = 0; j < 26; j++) {
+			if (c == al[j]) {
+				sum += pow(j, i);
+			}
+		}
+		return sum % length;
+	}
+	int hash(long long i) {
+		return i % length;
+	}
 	int hash(int i) {
 		return i % length;
 	}
@@ -126,9 +141,17 @@ public:
 		int res = man + pow(2, ord);
 		return res % length;
 	}
+	int hash(vector<int> v) {
+		int sum = 0;
+		for (int i = 0; i < v.size(); i++) {
+			sum += v[i];
+		}
+		return sum % length;
+	}
 	auto insert(TypeKey key, const TypeData& data) {
 		int pos = hash(key);
 		ar[pos].push_back(make_pair(key, data));
+		//checkcap();
 		for (auto it = ar[pos].begin(); it != ar[pos].end(); it++) {
 			if (it->first == key) {
 				return it;
@@ -170,7 +193,18 @@ public:
 	Iterator end() {
 		return Iterator(ar, length, 0, length);
 	}
-
+	void checkcap() {
+		int sum = 0;
+		for (int i = 0; i < length; i++) {
+			sum += ar[i].size();
+		}
+		double cap = sum / length;
+		if (cap > 1) {
+			cout<<balancecap(sum);
+		}
+	}
+	HashTable balancecap(int newLength) {
+	}
 	friend std::ostream& operator<<(std::ostream& out, const HashTable& v) {
 		for (int i = 0; i < v.length; i++) {
 			for (auto it = v.ar[i].begin(); it != v.ar[i].end(); it++) {
